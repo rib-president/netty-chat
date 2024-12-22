abfb33ffd1a32b0d74a8e6a78b369c4d584a552e
gi moon <pearlmoon997@gmail.com>
2024-12-22 13:18:30 +0900

    application

--- src/main/java/com/example/nettyChat/SocketServiceApplication.java
+++ src/main/java/com/example/nettyChat/SocketServiceApplication.java
@@ -1,15 +1,10 @@
 package com.example.nettyChat;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableWebFlux
-public class NettyChatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NettyChatApplication.class, args);
-	}
+public class SocketServiceApplication {
 
 }
