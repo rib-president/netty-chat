fe002c64528f46e9fd90b920425d10655fd14ede
gi moon <pearlmoon997@gmail.com>
2024-12-22 12:45:56 +0900

    init

---
+++ src/test/java/com/example/nettyChat/NettyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettyChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
